23025aedd828e9692e2d3506a2d0d6a32ac8253b
DarthHideous66 <khalvati.arvin@gmail.com>
2026-05-10 22:46:16 -0400

    Add files via upload
    I added a counter that counts the dirt tiles

--- scenes/wave_manager.gd
+++ scenes/wave_manager.gd
@@ -3,6 +3,8 @@ extends Node2D
 const ENEMY = preload("res://scenes/enemy.tscn")
 @onready var label = get_node("/root/World/Player/Label")
 @onready var enemy_label = get_node("/root/World/Player/enemy_label")
+@onready var survived_label = get_node("/root/World/Player/survived")
+@onready var dirts = get_node("/root/World/Player/dirts")
 var wave = 1
 var enemies_to_spawn = 5
 var enemies_spawned = 0
@@ -20,15 +22,18 @@ func start_wave():
 	enemies_spawned = 0
 	enemies_alive = enemies_to_spawn
 	$SpawnTimer.start(1)
+	survived_label.text = ""
 
 func update_ui():
 	label.text = "Wave: " + str(wave)
 	enemy_label.text = "Enemies left: " + str(enemies_alive)
+	dirts.text = "Dirt tiles left: " + str(get_tree().get_nodes_in_group("dirt").size())
 
 func enemy_died():
 	enemies_alive -= 1
 	if enemies_alive <= 0:
 		$WaveTimer.start(10)
+		survived_label.text = "You survived wave" + str(wave)
 
 func _on_spawn_timer_timeout() -> void:
 	var positions = [
