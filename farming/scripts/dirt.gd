bd353ae071ea7a404a18fbb68eea895f0508481c
DarthHideous66 <khalvati.arvin@gmail.com>
2026-05-10 21:05:28 -0400

    Add files via upload
    I know made it that you can press e while holding a carrot over a dug dirt to plant a carrot and then a couple secs later boom another carrot more health

---
+++ farming/scripts/dirt.gd
@@ -0,0 +1,87 @@
+extends Area2D
+
+@onready var anim = $AnimatedSprite2D
+@onready var timer =$Timer
+@onready var plant_anim = $plants
+@onready var plant_timer = $planted
+@onready var carrot = get_node("/root/World/carrot")
+var health = 0
+var dug = false
+var bodyy = null
+var is_inside = false
+var take_damage: bool = false
+var has_plant = false
+var start_plant_timer: bool = false
+const CARROT = preload("res://scenes/carrot.tscn")
+
+func _ready() -> void:
+	start_plant_timer = false
+	has_plant = false
+	health = 3
+	is_inside = false
+	anim.play("default")
+	dug = false
+	add_to_group("dirt")
+	take_damage = false
+
+
+func _process(delta: float) -> void:
+	start_plant_timer = false
+	if has_plant == false:
+		plant_anim.play("default")
+	if dug == true:
+		anim.play("plantable")
+	else:
+		anim.play("default")
+		
+	if is_inside:
+		if bodyy.equipped() == "shovel":
+			if Input.is_action_just_pressed("use"):
+				dig()
+				
+		elif bodyy.equipped() == "carrot":
+			if Input.is_action_just_pressed("plant"):
+				if dug:
+					plant_anim.play("has_plant")
+					has_plant = true
+					start_plant_timer = true
+	if start_plant_timer:
+		plant_timer.start(1)
+		
+				
+	if health == 0:
+		queue_free()
+
+func dig():
+	dug = true
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("player_identifier"):
+		is_inside = true
+		bodyy = body
+	elif body.has_method("enemy_identifier"):
+		timer.start(5)
+		take_damage = true
+
+func spawn_veggie(veggie):
+	var carrot = veggie.instantiate()
+	carrot.global_position = global_position
+	get_tree().current_scene.add_child(carrot)
+	has_plant = false
+
+func _on_body_exited(body: Node2D) -> void:
+	is_inside = false
+	if body.has_method("enemy_identifier"):
+		take_damage = false
+
+
+func _on_timer_timeout() -> void:
+	if take_damage:
+		health -= 1
+		if health > 0:
+			timer.start(5)
+
+
+func _on_planted_timeout() -> void:
+	spawn_veggie(CARROT)
+	plant_timer.stop()
